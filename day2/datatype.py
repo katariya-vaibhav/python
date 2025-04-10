7ca90dd48a79bd325ed5208f6e7d4ffe70438562
_coding__hub_ <143187581+katariya-vaibhav@users.noreply.github.com>
2025-04-10 16:44:43 +0530

    Add datatype.py with documentation on object types and data types

---
+++ day2/datatype.py
@@ -0,0 +1,45 @@
+# Object types / data types
+
+
+#  --- Numbers --- 
+# 123 , 22.2332 , 3+4j , 0b111 , Decimal() , Fraction()
+
+# --- Strings ---
+# 'hello' , "hello" , '''hello''' , """hello""" , str() , f"hello {name}"
+
+# --- Lists ---
+# [1, 2, 3] , [1, 2.2, 'hello'] , list() , [*range(5)] , [*'hello']
+
+# --- Tuples ---
+# (1, 2, 3) , (1, 2.2, 'hello') , tuple() , (*range(5)) , (*'hello')
+
+# dictionaries ---
+# {'key': 'value'} , {'key': 1, 'key2': 2} , dict() , dict(key='value')
+
+# --- Sets ---
+# {1, 2, 3} , {1, 2.2, 'hello'} , set() , {*range(5)} , {*'hello'}
+
+# --- Booleans ---
+# true , false
+
+# --- None ---
+# None -> null , empty , no value 
+
+# --- File ---
+# open('file.txt', 'r') , open('file.txt', 'w')
+
+# --- Functions ---
+# def function_name(): pass , lambda x: x*2 , map(), filter(), reduce()
+
+# --- Modules ---
+# import math , from math import sqrt , import os as operating_system 
+
+# --- Classes ---
+# class ClassName: pass , object() , type() , isinstance(), issubclass()
+
+# --- Exceptions ---
+# try: pass , except Exception as e: pass , raise Exception('Error'), finally: pass
+
+# advanced types
+# decorators , generators , iterators , metaProgramming
+
