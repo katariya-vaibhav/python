7085c551959192f643c210bc61417c88e3881f55
_coding__hub_ <143187581+katariya-vaibhav@users.noreply.github.com>
2025-04-14 17:31:31 +0530

    class and class decorator

--- day8_oops/01_oop.py
+++ day8_oops/01_oop.py
@@ -3,27 +3,47 @@
 # self is a reference to the current instance of the class. It is used to access the attributes and methods of the class.
 # The self parameter is always the first parameter of the __init__ method and any other methods in the class.
 
-
 class Car:
     
     total_car = 0 # Class variable to keep track of the number of car objects created
     
     def __init__(self , brand , model):
         self.__brand = brand
-        self.model = model
+        self.__model = model
         Car.total_car += 1
         
     def getBrand(self):
         return self.__brand
 
     def getCarInfo(self):
-        return f"{self.__brand} {self.model}"
+        return f"{self.__brand} {self.__model}"
     
     def fuelType(self):
         return "Petrol or Diesel"
+    
+    @staticmethod
+    def carDescription():
+        return f"car is used for transportation"
+    
+    @property
+    def brand(self):
+        return self.__brand
+    
+    @property
+    def model(self):
+        return self.__model
 
 
-class ElecticCar(Car):
+class Battery:
+    def info(self):
+        return "Battery information"
+
+class Engine:
+    def engineInfo(self):
+        return "Engine information"
+
+
+class ElecticCar(Car, Battery, Engine):
     def __init__(self , brand , model , battery_size):
         super().__init__(brand , model)
         self.battery_size = battery_size
@@ -43,16 +63,27 @@ class ElecticCar(Car):
 # print(my_car.getCarInfo()) # Toyota Corolla
 
 
-# my_electric_car = ElecticCar("Tesla" , "Model S" , 100)
+my_electric_car = ElecticCar("Tesla" , "Model S" , 100)
+print(my_electric_car.info())
+print(my_electric_car.engineInfo())
 # print(my_electric_car.brand) # Tesla    
 # print(my_electric_car.model) # Model S
 # print(my_electric_car.battery_size) # 100
 # print(my_electric_car.getCarInfo()) # Tesla Model S with 100 kWh battery
 
-my_electric_car = ElecticCar("Tesla" , "Model S" , 100)
-print(my_electric_car.fuelType())
+# my_electric_car = ElecticCar("Tesla" , "Model S" , 100)
+# print(my_electric_car.fuelType())
+# print(isinstance(my_electric_car , Car)) # True
+# print(isinstance(my_electric_car , ElecticCar)) # True
 
-my_car = Car("Toyota" , "Corolla")
-print(my_car.fuelType())
 
-print(Car.total_car)
+# my_car = Car("Toyota" , "Corolla")
+# print(my_car.fuelType())
+# print(my_car.carDescription())
+# print(Car.carDescription())
+# my_car.brand = "Honda" # This will raise an error because brand is a private attribute
+
+# print(my_car.brand)
+# print(my_car.model)
+# print(isinstance(my_car , Car)) #- True
+# print(isinstance(my_car , ElecticCar)) # - False
